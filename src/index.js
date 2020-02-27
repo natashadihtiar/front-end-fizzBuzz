57845da449d075ddc81819e74588a116161fb657
natashadihtiar <47831860+natashadihtiar@users.noreply.github.com>
2020-02-27 15:20:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function fizzBuzz(number) {
-    for (let i = 1; i < number; i++) {
+    for (let i = 1; i <= number; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
             console.log("fizzbuzz");
         }        
